9973d0dc52874b4cbc5988127d48a5d4903f3ffa
Devendra Milmile <d.h.milmile1@gmail.com>
2023-09-05 22:16:59 +0530

    Added angular library project

---
+++ projects/component-library/src/lib/component-library.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cl-component-library',
+  template: `
+    <p>
+      component-library works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ComponentLibraryComponent {
+
+}
